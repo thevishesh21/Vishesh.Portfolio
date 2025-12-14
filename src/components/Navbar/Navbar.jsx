0bc7095391ba7abfe9cb286857bb07b0f34c3690
Vishesh Pal <visheshpal4629@gmail.com>
2025-12-14 15:48:45 +0530

    React version

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+// eslint-disable-next-line no-unused-vars
 import { motion, AnimatePresence } from 'framer-motion';
 import './Navbar.css';
 
