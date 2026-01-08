8eac2eeae59f6b216eb5d78d346576e4aa15eaf2
thevishesh21 <visheshpal4629@gmail.com>
2026-01-08 19:09:27 +0530

    Conet frontend and backend

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
     setIsLoading(true);
     
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/contact/', {
+      const response = await fetch('https://api.vishesh-dev.me/api/contact/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
