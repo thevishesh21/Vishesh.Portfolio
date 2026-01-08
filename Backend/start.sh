1e7a31e74c475d2775eb9cdec2a2d8d79b46ca01
thevishesh21 <visheshpal4629@gmail.com>
2026-01-08 18:07:01 +0530

    your message

--- Backend/start.sh
+++ Backend/start.sh
@@ -3,4 +3,10 @@ set -o errexit
 
 python manage.py migrate
 python manage.py collectstatic --noinput
+
+python manage.py createsuperuser \
+  --noinput \
+  --username "$DJANGO_SUPERUSER_USERNAME" \
+  --email "$DJANGO_SUPERUSER_EMAIL" || true
+
 gunicorn backend.wsgi:application
